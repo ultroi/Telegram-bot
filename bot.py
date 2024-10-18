e7e902ba4f8852bec35216728670cc214696808d
Akshay Singh <86416271+ultroi@users.noreply.github.com>
2024-10-18 15:33:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -183,7 +183,7 @@ async def single_player_move(update: Update, _: ContextTypes.DEFAULT_TYPE) -> No
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
-    await query.edit_message_text(f"**You chose {player_choice}.**\n **Bot chose {bot_choice}.**\n **{result}**", reply_markup=reply_markup)
+    await query.edit_message_text(f"**You chose **{player_choice}.\n **Bot chose **{bot_choice}.\n *{result}*", reply_markup=reply_markup)
 
 
 # Async function for multiplayer mode
