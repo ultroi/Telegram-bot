83f577cc081e26b21eae15879699ce4977b82fb9
Akshay Singh <86416271+ultroi@users.noreply.github.com>
2024-10-18 16:05:40 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -294,7 +294,7 @@ async def show_dev_stats(update: Update, _: ContextTypes.DEFAULT_TYPE) -> None:
 
 # Main function to run the bot
 def main():
-    token = os.getenv("7441832203:AAEV96F3k9qIH3rm-LyTUfG-0kTl_CeN4Lg")
+    token = "7441832203:AAEV96F3k9qIH3rm-LyTUfG-0kTl_CeN4Lg"
     application = Application.builder().token(token).build()
 
     # Command handlers
