5d8481fcce1f346215391f1774c4e8e34ced3c30
Akshay Singh <86416271+ultroi@users.noreply.github.com>
2025-01-31 10:49:47 +0530

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,5 @@
-from .mode_selection import start, handle_move, handle_callback_query
+from .mode_selection import start, handle_callback_query
 from .show_stats import show_stats
 from .help_command import help_command as help
 from .multiplayer import start_multiplayer, join_multiplayer, handle_multiplayer_move, handle_game_end, matchmaking_process
-from .single_player import start_single_player  # Ensure this function is defined and imported
+from .single_player import start_single_player, single_player_move  # Ensure this function is defined and imported
