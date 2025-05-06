b337e4dd87bed87650fb6852b49a8f6cf3eab1d6
Akshay Singh <86416271+ultroi@users.noreply.github.com>
2025-05-07 00:10:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from handlers.mod import stats, leaderboard, achievements_callback, back_to_stat
 from handlers.challenge import challenge, challenge_callback, move_callback, clear_challenges_command, handle_rematch
 from handlers.data import manage_data_command, manage_data_callback
 from database.connection import ensure_tables_exist, migrate_stats
-from handlers.group_handler import ChatMemberHandler
+from handlers.group_handler import chat_member_update
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
